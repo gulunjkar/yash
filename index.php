aad70ad0a59f8d7546aff1b9d82be31507bd1a14
gulunjkar <yash.gulunjkar@mitwpu.edu.in>
2024-08-21 10:32:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <form action="insert.php" method="post">
+        <label>Name:</label> <input type="text" name="name"> <br>
+        <label>Email:</label> <input type="email" name="email"> <br>
+        <label>Age:</label> <input type="text" name="age"> <br>
+        <label>Address:</label> <input type="text" name="address"> <br>
+        <button type="submit" name="submit">Submit</button>
+    </form>
+</body>
+</html>
